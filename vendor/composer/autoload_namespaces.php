0bd8c93cc0263f5ec4a88f46ea59e4844ff90b75
chynoranskypatrik@gmail.com <p245990CH>
2018-07-23 11:27:14 +0200

    Google OAuth - added params to session

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,4 +8,6 @@ $baseDir = dirname($vendorDir);
 return array(
     'Pimple' => array($vendorDir . '/pimple/pimple/src'),
     'Mailgun' => array($vendorDir . '/mailgun/mailgun-php/src'),
+    'Google_Service_' => array($vendorDir . '/google/apiclient-services/src'),
+    'Google_' => array($vendorDir . '/google/apiclient/src'),
 );
