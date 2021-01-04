f3c5f31eca0cdb4ec8ba8fc67106e4edd0be8ddd
Christian Knoll <christian.knoll@twentyreasons.com>
2021-01-04 16:57:40 +0100

    core: add autoloader custom/KREST

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -18,6 +18,7 @@ return array(
     'SpiceCRM\\data\\' => array($baseDir . '/data'),
     'SpiceCRM\\custom\\modules\\' => array($baseDir . '/custom/modules'),
     'SpiceCRM\\custom\\includes\\' => array($baseDir . '/custom/include'),
+    'SpiceCRM\\custom\\KREST\\' => array($baseDir . '/custom/KREST'),
     'SpiceCRM\\KREST\\' => array($baseDir . '/KREST'),
     'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'SendGrid\\' => array($vendorDir . '/sendgrid/php-http-client/lib'),
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,7 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9961db7456b6021e867a4aee4ea34352
 {
-    public static $files = array (
+    public static $files = array(
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         'ddc0a4d7e61c0286f0f8593b1903e894' => __DIR__ . '/..' . '/clue/stream-filter/src/functions.php',
@@ -21,285 +21,289 @@ class ComposerStaticInit9961db7456b6021e867a4aee4ea34352
         '04a9a7b26fb79fbd712347cc9bc9d9be' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/stats/Stats.php',
     );
 
-    public static $prefixLengthsPsr4 = array (
-        'l' => 
-        array (
-            'libphonenumber\\' => 15,
-        ),
-        'j' => 
-        array (
-            'jamesiarmes\\PhpNtlm\\' => 20,
-            'jamesiarmes\\PhpEws\\' => 19,
-        ),
-        'W' => 
-        array (
-            'Webmozart\\Assert\\' => 17,
-        ),
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Ctype\\' => 23,
-            'Symfony\\Component\\Yaml\\' => 23,
-            'Symfony\\Component\\OptionsResolver\\' => 34,
-            'SpiceCRM\\modules\\' => 17,
-            'SpiceCRM\\includes\\' => 18,
-            'SpiceCRM\\data\\' => 14,
-            'SpiceCRM\\custom\\modules\\' => 24,
-            'SpiceCRM\\custom\\includes\\' => 25,
-            'SpiceCRM\\KREST\\' => 15,
-            'Slim\\' => 5,
-            'SendGrid\\' => 9,
-        ),
-        'R' => 
-        array (
-            'Ramsey\\Uuid\\' => 12,
-        ),
-        'P' => 
-        array (
-            'Psr\\Log\\' => 8,
-            'Psr\\Http\\Message\\' => 17,
-            'Psr\\Container\\' => 14,
-        ),
-        'I' => 
-        array (
-            'Interop\\Container\\' => 18,
-            'IXR\\' => 4,
-        ),
-        'H' => 
-        array (
-            'Http\\Promise\\' => 13,
-            'Http\\Message\\MultipartStream\\' => 29,
-            'Http\\Message\\' => 13,
-            'Http\\Discovery\\' => 15,
-            'Http\\Client\\Curl\\' => 17,
-            'Http\\Client\\Common\\' => 19,
-            'Http\\Client\\' => 12,
-            'Hfig\\MAPI\\' => 10,
-        ),
-        'G' => 
-        array (
-            'GuzzleHttp\\Psr7\\' => 16,
-            'GuzzleHttp\\Promise\\' => 19,
-            'GuzzleHttp\\' => 11,
-            'Giggsey\\Locale\\' => 15,
-        ),
-        'F' => 
-        array (
-            'FontLib\\' => 8,
-            'FastRoute\\' => 10,
-        ),
-        'D' => 
-        array (
-            'Dompdf\\' => 7,
-        ),
-        'C' => 
-        array (
-            'Clue\\StreamFilter\\' => 18,
-        ),
+    public static $prefixLengthsPsr4 = array(
+        'l' =>
+            array(
+                'libphonenumber\\' => 15,
+            ),
+        'j' =>
+            array(
+                'jamesiarmes\\PhpNtlm\\' => 20,
+                'jamesiarmes\\PhpEws\\' => 19,
+            ),
+        'W' =>
+            array(
+                'Webmozart\\Assert\\' => 17,
+            ),
+        'S' =>
+            array(
+                'Symfony\\Polyfill\\Ctype\\' => 23,
+                'Symfony\\Component\\Yaml\\' => 23,
+                'Symfony\\Component\\OptionsResolver\\' => 34,
+                'SpiceCRM\\modules\\' => 17,
+                'SpiceCRM\\includes\\' => 18,
+                'SpiceCRM\\data\\' => 14,
+                'SpiceCRM\\custom\\modules\\' => 24,
+                'SpiceCRM\\custom\\includes\\' => 25,
+                'SpiceCRM\\KREST\\' => 15,
+                'Slim\\' => 5,
+                'SendGrid\\' => 9,
+            ),
+        'R' =>
+            array(
+                'Ramsey\\Uuid\\' => 12,
+            ),
+        'P' =>
+            array(
+                'Psr\\Log\\' => 8,
+                'Psr\\Http\\Message\\' => 17,
+                'Psr\\Container\\' => 14,
+            ),
+        'I' =>
+            array(
+                'Interop\\Container\\' => 18,
+                'IXR\\' => 4,
+            ),
+        'H' =>
+            array(
+                'Http\\Promise\\' => 13,
+                'Http\\Message\\MultipartStream\\' => 29,
+                'Http\\Message\\' => 13,
+                'Http\\Discovery\\' => 15,
+                'Http\\Client\\Curl\\' => 17,
+                'Http\\Client\\Common\\' => 19,
+                'Http\\Client\\' => 12,
+                'Hfig\\MAPI\\' => 10,
+            ),
+        'G' =>
+            array(
+                'GuzzleHttp\\Psr7\\' => 16,
+                'GuzzleHttp\\Promise\\' => 19,
+                'GuzzleHttp\\' => 11,
+                'Giggsey\\Locale\\' => 15,
+            ),
+        'F' =>
+            array(
+                'FontLib\\' => 8,
+                'FastRoute\\' => 10,
+            ),
+        'D' =>
+            array(
+                'Dompdf\\' => 7,
+            ),
+        'C' =>
+            array(
+                'Clue\\StreamFilter\\' => 18,
+            ),
     );
 
-    public static $prefixDirsPsr4 = array (
-        'libphonenumber\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/giggsey/libphonenumber-for-php/src',
-        ),
-        'jamesiarmes\\PhpNtlm\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/jamesiarmes/php-ntlm/src',
-        ),
-        'jamesiarmes\\PhpEws\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-ews/php-ews/src',
-        ),
-        'Webmozart\\Assert\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/webmozart/assert/src',
-        ),
-        'Symfony\\Polyfill\\Ctype\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
-        ),
-        'Symfony\\Component\\Yaml\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/yaml',
-        ),
-        'Symfony\\Component\\OptionsResolver\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/options-resolver',
-        ),
-        'SpiceCRM\\modules\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/modules',
-        ),
-        'SpiceCRM\\includes\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/include',
-        ),
-        'SpiceCRM\\data\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/data',
-        ),
-        'SpiceCRM\\custom\\modules\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/custom/modules',
-        ),
-        'SpiceCRM\\custom\\includes\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/custom/include',
-        ),
-        'SpiceCRM\\KREST\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/KREST',
-        ),
-        'Slim\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/slim/slim/Slim',
-        ),
-        'SendGrid\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
-        ),
-        'Ramsey\\Uuid\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/ramsey/uuid/src',
-        ),
-        'Psr\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
-        ),
-        'Psr\\Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'Psr\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/container/src',
-        ),
-        'Interop\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
-        ),
-        'IXR\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/kissifrot/php-ixr/src',
-        ),
-        'Http\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/promise/src',
-        ),
-        'Http\\Message\\MultipartStream\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/multipart-stream-builder/src',
-        ),
-        'Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/message/src',
-            1 => __DIR__ . '/..' . '/php-http/message-factory/src',
-        ),
-        'Http\\Discovery\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/discovery/src',
-        ),
-        'Http\\Client\\Curl\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/curl-client/src',
-        ),
-        'Http\\Client\\Common\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/client-common/src',
-        ),
-        'Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/httplug/src',
-        ),
-        'Hfig\\MAPI\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/hfig/mapi/src/MAPI',
-        ),
-        'GuzzleHttp\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
-        ),
-        'GuzzleHttp\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
-        ),
-        'GuzzleHttp\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
-        ),
-        'Giggsey\\Locale\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/giggsey/locale/src',
-        ),
-        'FontLib\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phenx/php-font-lib/src/FontLib',
-        ),
-        'FastRoute\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/nikic/fast-route/src',
-        ),
-        'Dompdf\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/dompdf/dompdf/src',
-        ),
-        'Clue\\StreamFilter\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/clue/stream-filter/src',
-        ),
+    public static $prefixDirsPsr4 = array(
+        'libphonenumber\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/giggsey/libphonenumber-for-php/src',
+            ),
+        'jamesiarmes\\PhpNtlm\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/jamesiarmes/php-ntlm/src',
+            ),
+        'jamesiarmes\\PhpEws\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-ews/php-ews/src',
+            ),
+        'Webmozart\\Assert\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/webmozart/assert/src',
+            ),
+        'Symfony\\Polyfill\\Ctype\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+            ),
+        'Symfony\\Component\\Yaml\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/symfony/yaml',
+            ),
+        'Symfony\\Component\\OptionsResolver\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/symfony/options-resolver',
+            ),
+        'SpiceCRM\\modules\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/modules',
+            ),
+        'SpiceCRM\\includes\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/include',
+            ),
+        'SpiceCRM\\data\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/data',
+            ),
+        'SpiceCRM\\custom\\modules\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/custom/modules',
+            ),
+        'SpiceCRM\\custom\\includes\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/custom/include',
+            ),
+        'SpiceCRM\\KREST\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/KREST',
+            ),
+        'SpiceCRM\\custom\\KREST\\' =>
+            array(
+                0 => __DIR__ . '/../..' . '/custom/KREST',
+            ),
+        'Slim\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/slim/slim/Slim',
+            ),
+        'SendGrid\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
+            ),
+        'Ramsey\\Uuid\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/ramsey/uuid/src',
+            ),
+        'Psr\\Log\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+            ),
+        'Psr\\Http\\Message\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/psr/http-message/src',
+            ),
+        'Psr\\Container\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/psr/container/src',
+            ),
+        'Interop\\Container\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
+            ),
+        'IXR\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/kissifrot/php-ixr/src',
+            ),
+        'Http\\Promise\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/promise/src',
+            ),
+        'Http\\Message\\MultipartStream\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/multipart-stream-builder/src',
+            ),
+        'Http\\Message\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/message/src',
+                1 => __DIR__ . '/..' . '/php-http/message-factory/src',
+            ),
+        'Http\\Discovery\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/discovery/src',
+            ),
+        'Http\\Client\\Curl\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/curl-client/src',
+            ),
+        'Http\\Client\\Common\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/client-common/src',
+            ),
+        'Http\\Client\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/php-http/httplug/src',
+            ),
+        'Hfig\\MAPI\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/hfig/mapi/src/MAPI',
+            ),
+        'GuzzleHttp\\Psr7\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+            ),
+        'GuzzleHttp\\Promise\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+            ),
+        'GuzzleHttp\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+            ),
+        'Giggsey\\Locale\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/giggsey/locale/src',
+            ),
+        'FontLib\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/phenx/php-font-lib/src/FontLib',
+            ),
+        'FastRoute\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/nikic/fast-route/src',
+            ),
+        'Dompdf\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/dompdf/dompdf/src',
+            ),
+        'Clue\\StreamFilter\\' =>
+            array(
+                0 => __DIR__ . '/..' . '/clue/stream-filter/src',
+            ),
     );
 
-    public static $prefixesPsr0 = array (
-        'S' => 
-        array (
-            'Svg\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/phenx/php-svg-lib/src',
+    public static $prefixesPsr0 = array(
+        'S' =>
+            array(
+                'Svg\\' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/phenx/php-svg-lib/src',
+                    ),
+                'Sabberworm\\CSS' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/lib',
+                    ),
             ),
-            'Sabberworm\\CSS' => 
-            array (
-                0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/lib',
+        'P' =>
+            array(
+                'Pimple' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/pimple/pimple/src',
+                    ),
+                'PEAR' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/pear/pear_exception',
+                    ),
             ),
-        ),
-        'P' => 
-        array (
-            'Pimple' => 
-            array (
-                0 => __DIR__ . '/..' . '/pimple/pimple/src',
+        'O' =>
+            array(
+                'OLE' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/pear/ole',
+                    ),
             ),
-            'PEAR' => 
-            array (
-                0 => __DIR__ . '/..' . '/pear/pear_exception',
+        'M' =>
+            array(
+                'Mailgun' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/mailgun/mailgun-php/src',
+                    ),
             ),
-        ),
-        'O' => 
-        array (
-            'OLE' => 
-            array (
-                0 => __DIR__ . '/..' . '/pear/ole',
+        'C' =>
+            array(
+                'Console' =>
+                    array(
+                        0 => __DIR__ . '/..' . '/pear/console_getopt',
+                    ),
             ),
-        ),
-        'M' => 
-        array (
-            'Mailgun' => 
-            array (
-                0 => __DIR__ . '/..' . '/mailgun/mailgun-php/src',
-            ),
-        ),
-        'C' => 
-        array (
-            'Console' => 
-            array (
-                0 => __DIR__ . '/..' . '/pear/console_getopt',
-            ),
-        ),
     );
 
-    public static $fallbackDirsPsr0 = array (
+    public static $fallbackDirsPsr0 = array(
         0 => __DIR__ . '/..' . '/pear/pear-core-minimal/src',
     );
 
-    public static $classMap = array (
+    public static $classMap = array(
         'Cpdf' => __DIR__ . '/..' . '/dompdf/dompdf/lib/Cpdf.php',
         'HTML5_Data' => __DIR__ . '/..' . '/dompdf/dompdf/lib/html5lib/Data.php',
         'HTML5_InputStream' => __DIR__ . '/..' . '/dompdf/dompdf/lib/html5lib/InputStream.php',
