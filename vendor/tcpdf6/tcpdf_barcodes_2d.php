9e7aee68b4e2fa9ce69a7259d4e185813ab1f910
sebastian hautz <76937890+sebastianhautz@users.noreply.github.com>
2021-02-16 17:04:37 +0100

    .) changed from array() to [] .) changed some formating

--- vendor/tcpdf6/tcpdf_barcodes_2d.php
+++ vendor/tcpdf6/tcpdf_barcodes_2d.php
@@ -256,7 +256,7 @@ class TCPDF2DBarcode {
 			case 'DATAMATRIX': { // DATAMATRIX (ISO/IEC 16022)
 				require_once(dirname(__FILE__).'/include/barcodes/datamatrix.php');
 				$qrcode = new Datamatrix($code);
-				$this->barcode_array = $qrcode->getBarcodeArray();
+				$this->barcode_array = $qrcode->getBarcode[];
 				$this->barcode_array['code'] = $code;
 				break;
 			}
@@ -273,7 +273,7 @@ class TCPDF2DBarcode {
 					$ecl = intval($mode[2]);
 				}
 				// set macro block
-				$macro = array();
+				$macro = [];
 				if (isset($mode[3]) AND ($mode[3] !== '') AND isset($mode[4]) AND ($mode[4] !== '') AND isset($mode[5]) AND ($mode[5] !== '')) {
 					$macro['segment_total'] = intval($mode[3]);
 					$macro['segment_index'] = intval($mode[4]);
@@ -287,7 +287,7 @@ class TCPDF2DBarcode {
 					}
 				}
 				$qrcode = new PDF417($code, $ecl, $aspectratio, $macro);
-				$this->barcode_array = $qrcode->getBarcodeArray();
+				$this->barcode_array = $qrcode->getBarcode[];
 				$this->barcode_array['code'] = $code;
 				break;
 			}
@@ -297,7 +297,7 @@ class TCPDF2DBarcode {
 					$mode[1] = 'L'; // Ddefault: Low error correction
 				}
 				$qrcode = new QRcode($code, strtoupper($mode[1]));
-				$this->barcode_array = $qrcode->getBarcodeArray();
+				$this->barcode_array = $qrcode->getBarcode[];
 				$this->barcode_array['code'] = $code;
 				break;
 			}
@@ -318,7 +318,7 @@ class TCPDF2DBarcode {
 				}
 				$this->barcode_array['num_rows'] = count($rows);
 				$this->barcode_array['num_cols'] = strlen($rows[0]);
-				$this->barcode_array['bcode'] = array();
+				$this->barcode_array['bcode'] = [];
 				foreach ($rows as $r) {
 					$this->barcode_array['bcode'][] = str_split($r, 1);
 				}
