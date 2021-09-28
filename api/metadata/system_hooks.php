7636e4f822fb35f079ba1f07005e397eba802319
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2021-09-28 09:54:29 +0200

    Added hook_include field in syshooks
    backward compatibility

--- api/metadata/system_hooks.php
+++ api/metadata/system_hooks.php
@@ -20,6 +20,11 @@ $dictionary['syshooks'] = [
             'name' => 'hook_index',
             'type' => 'int',
         ],
+        'hook_include' => [
+            'name' => 'hook_include',
+            'type' => 'varchar',
+            'len' => 100
+        ],
         'hook_class' => [
             'name' => 'hook_class',
             'type' => 'varchar',
@@ -85,6 +90,11 @@ $dictionary['syscustomhooks'] = [
         'hook_index' => [
             'name' => 'hook_index',
             'type' => 'int',
+        ],
+        'hook_include' => [
+            'name' => 'hook_include',
+            'type' => 'varchar',
+            'len' => 100
         ],
         'hook_class' => [
             'name' => 'hook_class',
