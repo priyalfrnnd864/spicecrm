82bf73d5384a5d8bd2330e583814efad71052d01
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2021-12-06 08:33:46 +0100

    adds sendinblue mapping table

--- api/metadata/system_ui.php
+++ api/metadata/system_ui.php
@@ -2935,3 +2935,35 @@ $dictionary['sysevalanche_fieldmapping'] = [
         ]
     ]
 ];
+$dictionary['syssendinblue_fieldmapping'] = [
+    'table' => 'syssendinblue_fieldmapping',
+    'audited' => true,
+    'fields' => [
+        'id' => [
+            'name' => 'id',
+            'type' => 'id'
+        ],
+        'module' => [
+            'name' => 'module',
+            'type' => 'varchar',
+            'len' => 255
+        ],
+        'spice' => [
+            'name' => 'spice',
+            'type' => 'varchar',
+            'len' => 255
+        ],
+        'sendinblue' => [
+            'name' => 'sendinblue',
+            'type' => 'varchar',
+            'len' => 255
+        ]
+    ],
+    'indices' => [
+        [
+            'name' => 'idx_syssendinblue_fieldmapping',
+            'type' => 'primary',
+            'fields' => ['id']
+        ]
+    ]
+];
