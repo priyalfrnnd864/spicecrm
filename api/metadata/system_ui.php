215af2ee1dde33b827f01f6537e74f3663e4a24a
Christian Knoll <christian.g.knoll@gmail.com>
2022-03-22 19:39:41 +0100

    new feature asset manager

--- api/metadata/system_ui.php
+++ api/metadata/system_ui.php
@@ -2,6 +2,34 @@
 /***** SPICE-HEADER-SPACEHOLDER *****/
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionaryHandler;
 
+
+SpiceDictionaryHandler::getInstance()->dictionary['sysuiassets'] = [
+    'table' => 'sysuiassets',
+    'audited' => true,
+    'fields' => [
+        'id' => [
+            'name' => 'id',
+            'type' => 'id'
+        ],
+        'assetkey' => [
+            'name' => 'assetkey',
+            'type' => 'id'
+        ],
+        'assetvalue' => [
+            'name' => 'assetvalue',
+            'type' => 'longtext'
+        ],
+    ],
+    'indices' => [
+        [
+            'name' => 'idx_sysuiassets',
+            'type' => 'primary',
+            'fields' => ['id']
+        ]
+    ]
+];
+
+
 SpiceDictionaryHandler::getInstance()->dictionary['systextids_modules'] = [
     'table' => 'systextids_modules',
     'audited' => true,
