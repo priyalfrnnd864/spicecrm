f706510a0724f4b2124a9ac5dd03234e01734eea
maretval <valerie.maret@twentyreasons.com>
2023-01-28 11:35:25 +0100

    CR1000882: set duplicate_merge property in field type link

--- api/metadata/system_vardefs.php
+++ api/metadata/system_vardefs.php
@@ -931,6 +931,11 @@ SpiceDictionaryHandler::getInstance()->dictionary['sysdictionaryrelationships'] 
             'default' => false,
             'comment' => 'load link per default'
         ],
+        'lhs_duplicatemerge' => [
+            'name' => 'lhs_duplicatemerge',
+            'vname' => 'LBL_DUPLICTAE_MERGE',
+            'type' => 'bool',
+        ],
         'rhs_sysdictionarydefinition_id' => [
             'name' => 'rhs_sysdictionarydefinition_id',
             'vname' => 'LBL_RHS_TABLE',
@@ -971,6 +976,11 @@ SpiceDictionaryHandler::getInstance()->dictionary['sysdictionaryrelationships'] 
             'type' => 'bool',
             'default' => false,
             'comment' => 'load link per default'
+        ],
+        'rhs_duplicatemerge' => [
+            'name' => 'rhs_duplicatemerge',
+            'vname' => 'LBL_DUPLICTAE_MERGE',
+            'type' => 'bool',
         ],
         'rhs_relatename' => [
             'name' => 'rhs_relatename',
