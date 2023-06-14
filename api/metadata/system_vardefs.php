1a1b157c0a83909993d5f9088723a90ef6de7f93
maretval <valerie.maret@twentyreasons.com>
2023-06-14 11:37:07 +0200

    sysdictionaryfields: increased length for fieldtype

--- api/metadata/system_vardefs.php
+++ api/metadata/system_vardefs.php
@@ -1284,7 +1284,7 @@ SpiceDictionaryHandler::getInstance()->dictionary['sysdictionaryfields'] = [
         'fieldtype' => [
             'name' => 'fieldtype',
             'type' => 'varchar',
-            'len' => 20,
+            'len' => 32,
             'comment' => 'the field ytpe'
         ],
         'fielddefinition' => [
