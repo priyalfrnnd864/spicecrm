a205815e5b398fbecc27a8cecf561fd530a8bc63
maretval <valerie.maret@twentyreasons.com>
2024-04-05 13:53:29 +0200

    Dictionary kreportcategories: added index idx_kreportcategories_del

--- api/metadata/kreport.categories.metadata.php
+++ api/metadata/kreport.categories.metadata.php
@@ -159,6 +159,7 @@ SpiceDictionaryHandler::getInstance()->dictionary['kreportcategories'] = [
     ],
     'indices' => [
         ['name' => 'kreportcategoriespk', 'type' => 'primary', 'fields' => ['id']],
+        ['name' => 'idx_kreportcategories_del', 'type' => 'index', 'fields' => ['deleted']],
         ['name' => 'idx_is_admin', 'type' => 'index', 'fields' => ['is_admin_only', 'deleted']],
     ],
 ];
