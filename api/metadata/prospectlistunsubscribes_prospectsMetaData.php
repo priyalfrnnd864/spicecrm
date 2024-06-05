f4020c7135eb8fca07b5e2fb08898f52f1cf91c8
Maria Zakharova <maria.zakharova@twentyreasons.com>
2024-06-05 16:20:31 +0200

    CR1001326 changes for sendgrid to handle multiemail in prospectlists

--- api/metadata/prospectlistunsubscribes_prospectsMetaData.php
+++ api/metadata/prospectlistunsubscribes_prospectsMetaData.php
@@ -34,6 +34,11 @@ SpiceDictionaryHandler::getInstance()->dictionary['prospectlistunsubscribes_pros
             'name' => 'deleted',
             'type' => 'bool',
             'default' => '0'
+        ],
+        [
+            'name' => 'email_addr_bean_rel_id',
+            'type' => 'varchar',
+            'len' => '36',
         ]
     ],
     'indices' => [
--- api/modules/ProspectListUnsubscribes/vardefs.php
+++ api/modules/ProspectListUnsubscribes/vardefs.php
@@ -20,12 +20,24 @@ SpiceDictionaryHandler::getInstance()->dictionary['ProspectListUnsubscribe'] = [
             'type' => 'link',
             'relationship' => 'prospectlistunsubscribes_contacts',
             'source' => 'non-db',
+            'rel_fields' => [
+                'email_addr_bean_rel_id' => [
+                    'map' => 'prospectlists_person_email_addr_bean_rel_id'
+                ]
+            ]
         ],
         'is_default_sendgrid' => [
             'name' => 'is_default_sendgrid',
             'vname' => 'LBL_ISDEFAULT_SENDGRID',
             'type' => 'bool',
             'default' => 0,
+        ],
+        'prospectlists_person_email_addr_bean_rel_id' => [
+            'name' => 'prospectlists_person_email_addr_bean_rel_id',
+            'vname' => 'LBL_EMAIL_ADDRESS',
+            'type' => 'varchar',
+            'len' => '36',
+            'source' => 'non-db'
         ],
 
     ]
